29e13872bd4e5b78ed15fec607ce26f398cf81d0
Manoj Pathak <manojpathak227@gmail.com>
2022-05-13 01:06:07 -0500

    add changes for the commands

--- all_commands.sh
+++ all_commands.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # First will create bucket which will be used for deployment
-aws s3 mb://deployment-lambda-pushproduct
+aws s3 mb s3://deployment-dbstreams-test
 
 # this is for packaging using cloudformation
 
-aws cloudformation package --template-file template.yml --s3-bucket deployment-lambda-pushproduct --s3-prefix myproject/ --output-template-file packaged.yml
+aws cloudformation package --template-file template.yml --s3-bucket deployment-dbstreams-test --s3-prefix myproject/ --output-template-file packaged.yml
 
 # Deployments for cloudformation stack
 aws cloudformation deploy --template-file /Users/manopath/Documents/workspace/custom_resources_trigger/packaged.yml --stack-name pushproduct --capabilities CAPABILITY_IAM
 
 
-# this command worked 
-aws cloudformation package --template-file template.yml --s3-bucket deployment-lambda-pushproduct 
+# this command worked
+aws cloudformation package --template-file template.yml --s3-bucket deployment-dbstreams-test
 
