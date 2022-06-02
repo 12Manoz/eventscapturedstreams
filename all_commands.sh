dfdcaac7b60235f6bc103e6f5b111a229f3bc5d6
Manoj Pathak <manojpathak227@gmail.com>
2022-06-02 15:37:09 -0500

    Add chagnes for the read me

--- all_commands.sh
+++ all_commands.sh
@@ -7,7 +7,7 @@ aws s3 mb s3://deployment-dbstreams-test
 aws cloudformation package --template-file template.yml --s3-bucket deployment-dbstreams-test --s3-prefix myproject/ --output-template-file packaged.yml
 
 # Deployments for cloudformation stack
-aws cloudformation deploy --template-file /Users/manopath/Documents/workspace/aws_sample_dbstreams/eventscapturedstreams/packaged.yml --stack-name dynamostreams --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file /Users/manopath/Documents/workspace/aws_sample_dbstreams/eventscapturedstreams/packaged.yml --stack-name eventcapturestreams --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 
 
 # this command worked
@@ -18,3 +18,10 @@ aws cloudformation delete-stack --stack-name dynamostreams
 
 ## details
 stream=arn:aws:dynamodb:us-east-1:554995069609:table/tbff2002_prod_autm_rvis/stream/2022-04-19T16:31:42.091
+
+
+## deployment of dynamodb 
+aws cloudformation package --template-file dbtemplate.yml --s3-bucket deployment-dbstreams-test --s3-prefix dbpacakage/ --output-template-file dbpackaged.yml
+aws cloudformation deploy --template-file /Users/manopath/Documents/workspace/aws_sample_dbstreams/eventscapturedstreams/dbpackaged.yml --stack-name notificationcenter --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+
+
